b1918c05d675c4540550c30d6be310b67cb62e2c
unknown <shtorm20300@gmail.com>
2024-11-22 15:56:58 +0300

    skillet_bot

---
+++ run.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from dotenv import load_dotenv
+import os
+
+from config import TOKEN
+
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer('//')
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except BaseException:
+        print("Бот выключен")
